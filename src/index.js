e6bebaf19d912b53c403ad85ed1add97b4f8b28e
Denis Matern <denismatern@gmail.com>
2024-07-24 15:08:13 +0300

    fix: settings and logic

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./styles/main.css";
-
-function component(text) {
-  const element = document.createElement('h1');
-  element.textContent = text;
-  return element;
-}
-
-document.body.prepend(component('Проект собран на Webpack'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,78 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const FileManagerPlugin = require("filemanager-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: path.join(__dirname, "src", "index.js"),
+    entry: path.join(__dirname, "src", "index.ts"),
     output: {
         path: path.join(__dirname, "dist"),
         filename: "index.[contenthash:8].js",
-        clean: true
+        assetModuleFilename: path.join("images", "[name].[contenthash][ext]")
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
     },
     module: {
         rules: [
             {
-                test: /\.js$/,
-                use: "babel-loader",
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                        options: {
+                            transpileOnly: true, // Не проверять типы в ts-loader
+                        }
+                    },
+                    {
+                        loader: "babel-loader"
+                    }
+                ],
                 exclude: /node_modules/
             },
             {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader", "postcss-loader"]
+                use: [
+                    MiniCssExtractPlugin.loader, // вместо "style-loader"
+                    "css-loader",
+                    "postcss-loader"
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: "asset/resource"
+            },
+            {
+                test: /\.svg$/,
+                type: "asset/resource",
+                generator: {
+                    filename: path.join("icons", "[name].[contenthash][ext]")
+                }
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, "src", "template.html"),
+            template: path.join(__dirname, "src", "index.html"),
             filename: "index.html"
         }),
         new FileManagerPlugin({
             events: {
                 onStart: {
-                delete: ['dist']
+                    delete: ['dist']
+                },
+                onEnd: {
+                    copy: [
+                        {
+                            source: path.join("public"),
+                            destination: "dist"
+                        }
+                    ]
                 }
             }
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css"
         })
     ],
     devServer: {
